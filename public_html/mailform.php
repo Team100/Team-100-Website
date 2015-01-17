fcc4b2fc3b3a298d11d2f0b5d61005142fa90f49
Fletcher <790746@seq.org>
2015-01-17 01:47:39 -0800

    mailform.php html Updated.
    Its the same as a html doc.

--- public_html/mailform.php
+++ public_html/mailform.php
@@ -24,10 +24,15 @@
     <![endif]-->
 <!-- Le fav and touch icons -->
 <link rel="shortcut icon" href="assets/ico/favicon.ico">
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+<link rel="apple-touch-icon" href="assets/ico/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="57x57" href="assets/ico/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="72x72" href="assets/ico/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="assets/ico/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="assets/ico/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="assets/ico/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="assets/ico/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="assets/ico/apple-touch-icon-152x152.png">
+<link rel="apple-touch-startup-image" href="assets/ico/startup.png">
 
 <style type="text/css">
 body {
@@ -95,34 +100,50 @@ body {
       <div class="nav-collapse collapse">
         <ul class="nav">
           <li><a href="first.html">FIRST</a></li>
-          <li><a href="team.html">Team</a></li>
+          <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Team<b class="caret"></b></a>
+		     <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+			<li role="menuitem"><a href="team.html">Team 100</a></li>
+	   <li role="menuitem"><a href="team.html#history">History</a></li>
+       <li role="menuitem"><a href="team.html#awards">Awards</a></li>
+       <li role="menuitem"><a href="team.html#sponsors">Sponsors</a></li>
+       <li role="menuitem"><a href="team.html#outreach">Outreach</a></li>
+       <li role="menuitem"><a href="team.html#mentors">Mentors and Volunteers</a></li>
+       <li role="menuitem"><a href="ftcteam.html">FTC Team 4800 and Team 4799</a></li>
+            </ul>
           <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Media<b class="caret"></b></a>
             <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-              <li><a href="videos.html">Videos</a></li>
+              <li role="menuitem" ><a href="photos.html">Photos</a></li>
+              <li role="menuitem" ><a href="videos.html">Videos</a></li>
 
             </ul>
           </li>
-          <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Info<b class="caret"></b></a>
-            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-              <li><a href="calendar.html">Calendar</a></li>
-
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </li>
+         
           <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Robots<b class="caret"></b></a>
             <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-        <li><a href="robots.html#Kraken">2012: The Kraken</a></li>
-        <li><a href="robots.html#Wrath">2011: G-Wrath</a></li>
-        <li><a href="robots.html#Hammerhead">2010: Hammerhead</a></li>
-        <li><a href="robots.html#LunaBeast">2009: LunaBeast</a></li>
-        <li><a href="robots.html#Frog">2008: Ball Frog</a></li>
-        <li><a href="robots.html#Tube">2007: Tube-a-saurus</a></li>
+        <li role="menuitem"><a href="robots.html#2015">2015: TBA</a></li>
+	   <li role="menuitem"><a href="robots.html#Ballrus">2014: Ballrus</a></li>
+       <li role="menuitem"><a href="robots.html#Orangahang">2013: Orangahang</a></li>
+       <li role="menuitem"><a href="robots.html#Kraken">2012: The Kraken</a></li>
+       <li role="menuitem"><a href="robots.html#Wrath">2011: G-Wrath</a></li>
+       <li role="menuitem"><a href="robots.html#Hammerhead">2010: Hammerhead</a></li>
+       <li role="menuitem"><a href="robots.html#LunaBeast">2009: LunaBeast</a></li>
+       <li role="menuitem"><a href="robots.html#Frog">2008: Ball Frog</a></li>
+       <li role="menuitem"><a href="robots.html#Tube">2007: Tube-a-saurus</a></li>
+            </ul>
+          </li>
+		   <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Info<b class="caret"></b></a>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+              <li role="menuitem"><a href="calendar.html">Calendar</a></li>
+              <!-- dba <li><a href="#">Member Benefits</a></li> -->
+              <li role="menuitem"><a href="contact.html">Contact Us</a></li>
             </ul>
           </li>
         </ul>
       </div>
       <!--/.nav-collapse -->
-      <img src="assets/img/FRC-ExcelAward'12.gif" class="pull-right"></div>
+    <img src="assets/img/FRC-ExcelAward'12.gif" class="pull-right" alt="FRC ExcelAward for 2012">
+	<img src="assets/img/FRC-ExcelAward'10.gif" class="pull-right" alt="FRC ExcelAward for 2010">
+	<img src="assets/img/FRC-ExcelAward'08.gif" class="pull-right" alt="FRC ExcelAward for 2008">
   </div>
 </div>
 <!-- /navigation -->
@@ -144,7 +165,7 @@ mail($email_to, $subject ,$message ) ;
 
 <p> Your information has been sent. We will reply soon.</p>
 
-<p>Again, thank you for your interest in the Team 100 Wildhats web site</p>
+<p>Again, thank you for your interest in the Team 100 Wildhats</p>
 
 <!-- I know the return to index link isn't necessary, but the one on the top bar isn't very clear -->
 <p><a href="index.html">Return to Index</a></p>
