3e90d92869649071c03630430ddbed7c63ac4d26
FS790746 <790746@seq.org>
2016-03-10 20:13:31 -0800

    Lot o' Updates

--- mailform.php
+++ mailform.php
@@ -126,7 +126,8 @@ body {
           </li>
 		  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Robots<b class="caret"></b></a>
             <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-			<li role="menuitem"><a href="robots#Slidewinder">2015: Slidewinder</a></li>
+	   <li role="menuitem"><a href="robots#BALLista">2016: BALLista</a></li>
+	   <li role="menuitem"><a href="robots#Slidewinder">2015: Slidewinder</a></li>
 	   <li role="menuitem"><a href="robots#Ballrus">2014: Ballrus</a></li>
        <li role="menuitem"><a href="robots#Orangahang">2013: Orangahang</a></li>
        <li role="menuitem"><a href="robots#Kraken">2012: The Kraken</a></li>
